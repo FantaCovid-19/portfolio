93f380a204c067a1cf2346d807ae2263d301a827
Mr.Fantasma <contact@mrfantasma.com>
2026-04-03 22:10:30 -0300

    feat(config): add new vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import VueRouter from 'vue-router/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    VueRouter({
+      routesFolder: {
+        src: 'src/router/routes',
+        path: '',
+        exclude: (excluded) => excluded,
+        filePatterns: (filePatterns) => filePatterns,
+        extensions: (extensions) => extensions,
+      },
+
+      extensions: ['.vue'],
+      filePatterns: ['**/*'],
+      exclude: [],
+    }),
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
