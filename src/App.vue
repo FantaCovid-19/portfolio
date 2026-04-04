b103f14eaaee703e99e67af52277b1507722d619
Mr.Fantasma <contact@mrfantasma.com>
2026-04-03 22:12:21 -0300

    feat: update app navigation and routing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
-<script setup lang="ts">
-import Navbar from './components/Navbar.vue'
-import Footer from './components/Footer.vue'
-import Hero from './components/Sections/Hero.vue'
-import About from './components/Sections/About.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <Navbar />
-  <main>
-    <Hero />
-    <About />
-  </main>
-  <Footer />
+  <RouterView />
 </template>
 
 <style>
